4fa957d170f9d5b51649f38a917d49e058149da8
kailua <kailua@project2by2.jp>
2026-03-30 19:27:38 +0900

    scripts: Fix python scripts

--- msm-4.9/scripts/gcc-wrapper.py
+++ msm-4.9/scripts/gcc-wrapper.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
@@ -52,10 +52,12 @@ ofile = None
 warning_re = re.compile(r'''(.*/|)([^/]+\.[a-z]+:\d+):(\d+:)? warning:''')
 def interpret_warning(line):
     """Decode the message from gcc.  The messages we care about have a filename, and a warning"""
+    if isinstance(line, bytes):
+        line = line.decode('utf-8', errors='replace')
     line = line.rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
-        print >> sys.stderr, "error, forbidden warning:", m.group(2)
+        print("error, forbidden warning:", m.group(2), file=sys.stderr)
 
         # If there is a warning, remove any object if it exists.
         if ofile:
@@ -80,17 +82,19 @@ def run_gcc():
     try:
         proc = subprocess.Popen(args, stderr=subprocess.PIPE)
         for line in proc.stderr:
-            print >> sys.stderr, line,
+            if isinstance(line, bytes):
+                line = line.decode('utf-8', errors='replace')
+            print(line, end='', file=sys.stderr)
             interpret_warning(line)
 
         result = proc.wait()
     except OSError as e:
         result = e.errno
         if result == errno.ENOENT:
-            print >> sys.stderr, args[0] + ':',e.strerror
-            print >> sys.stderr, 'Is your PATH set correctly?'
+            print(args[0] + ':', e.strerror, file=sys.stderr)
+            print('Is your PATH set correctly?', file=sys.stderr)
         else:
-            print >> sys.stderr, ' '.join(args), str(e)
+            print(' '.join(args), str(e), file=sys.stderr)
 
     return result
 
